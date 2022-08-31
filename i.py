ed753cbf56de5672ec380241a98316e9d3f3af34
Yashu_Krish11 <89150253+yashu1wwww@users.noreply.github.com>
2022-08-31 10:24:05 +0530

    Update i.py

--- i.py
+++ i.py
@@ -1,13 +1,34 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+from threading import Thread, Barrier
 
-driver = webdriver.Chrome()
-driver1 = webdriver.Chrome()
-driver2= webdriver.Chrome()
-driver3 = webdriver.Chrome()
 
-driver.get("https://in.bookmyshow.com/login/?referer=/bengaluru/movies/vikrant-rona/ET00128225")
-driver1.get("https://in.bookmyshow.com/login/?referer=/bengaluru/movies/vikrant-rona/ET00128225")
-driver2.get("https://in.bookmyshow.com/login/?referer=/bengaluru/movies/vikrant-rona/ET00128225")
-driver3.get("https://in.bookmyshow.com/login/?referer=/bengaluru/movies/vikrant-rona/ET00128225")
+def func(barrier):
+    
+    driver = webdriver.Chrome()
 
-#add more driver you want Fill The Mails For One By One 👍
+    driver.get("https://in.bookmyshow.com/bengaluru/movies/varisu/ET00332034") #for script referal i putted one movie url change to  to your required movie url 
+
+    print('wait for others')
+    barrier.wait()
+
+    print('click')
+    b.click()
+    
+   #below code was convenient to my script run so used it it available in stackoverflow 
+
+number_of_threads = 5 #change how much tab you want i entered 5 change to your required
+
+barrier = Barrier(number_of_threads)
+
+threads = []
+
+for _ in range(number_of_threads):
+    t = Thread(target=func, args=(barrier,)) 
+    t.start()
+    threads.append(t)
+
+for t in threads:
+    t.join()
